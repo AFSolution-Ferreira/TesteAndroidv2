94832cc8a69c39b1f3982133f1649c018e93bd8f
Alexandre Ferreira <afsolution.ferreira@gmail.com>
2018-11-07 23:05:14 -0200

    Init Fork, create page splash and import images resources

---
+++ SantanderApp/app/src/main/java/br/com/afsolution/br/testesantandeandroid/SplashActivity.java
@@ -0,0 +1,13 @@
+package br.com.afsolution.br.testesantandeandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
